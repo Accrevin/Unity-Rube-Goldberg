c2b0bd87daf545d5fc78f94cb2ceb34b3b5b2c75
Katie <Katieau.x@gmail.com>
2024-04-08 13:12:28 -0700

    Changed script to not cancel currently playinig clip when called and changed volume to be a slider

--- Assets/KatieScripts/PlayCollisionAudio.cs
+++ Assets/KatieScripts/PlayCollisionAudio.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class PlayCollisionAudio : MonoBehaviour
 {
-    [SerializeField] private AudioSource _AudioSource;
-    [SerializeField] private bool CanHit = false;
+
+    [Header("Audio Settings")]
+    [Tooltip("Make sure to set play on awake to be false")]
+    private AudioSource _AudioSource;
+    [Range(0.0f, 1.0f)]
+    [SerializeField] private float _Volume;
+    [SerializeField] private AudioClip _Clip;
+
+    private bool CanHit = false;
 
     void Start()
     {
@@ -15,10 +22,17 @@ public class PlayCollisionAudio : MonoBehaviour
     }
     private void OnCollisionEnter (Collision collision)
     {
+        //do not disable the currently playing clip when triggered
         if (collision != null && CanHit == true)
-        _AudioSource.Play();
+        _AudioSource.PlayOneShot(_Clip, _Volume);
+        else if (_Clip == null )
+        {
+            Debug.LogWarning($"{gameObject.name}: No audio clip found");
+        }
+
     }
 
+    //CanHit to prevent audio from playing on start
     IEnumerator SetCanHit()
     {
         yield return new WaitForSeconds(3);
